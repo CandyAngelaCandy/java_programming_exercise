b952469a5ec00efc48e79adfc0f69866529a3e39
Candy <229355694@qq.com>
2018-03-29 14:38:42 +0800

    implementation class in section3 PracticeATest

--- src/main/java/cn/school/thoughtworks/section3/PracticeA.java
+++ src/main/java/cn/school/thoughtworks/section3/PracticeA.java
@@ -6,7 +6,17 @@ import java.util.Map;
 public class PracticeA {
     Map<String,Integer> createUpdatedCollection(Map<String,Integer> collectionA, Map<String,List<String>> object) {
         //实现练习要求，并改写该行代码。
+        List<String> speValEleInObj = object.get("value");
 
-        return null;
+        for (String s1 : speValEleInObj) {
+            boolean isContains = collectionA.containsKey(s1);
+
+            if (isContains) {
+                int speEleNum = collectionA.get(s1) - 1;
+                collectionA.put(s1, speEleNum);
+            }
+        }
+
+        return collectionA;
     }
 }
