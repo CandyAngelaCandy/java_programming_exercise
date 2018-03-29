0885aae2513b6330420de6c66298d9bffbe97d0f
Candy <229355694@qq.com>
2018-03-29 10:57:14 +0800

    implementation class in section2 PracticeATest

--- src/main/java/cn/school/thoughtworks/section2/PracticeA.java
+++ src/main/java/cn/school/thoughtworks/section2/PracticeA.java
@@ -7,6 +7,20 @@ import java.util.Map;
 public class PracticeA {
     Map<String, Integer> countSameElements(List<String> collection1) {
         //实现练习要求，并改写该行代码。
-        return null;
+        Map<String, Integer> calSameEleNumInc1 = new HashMap<String, Integer>();
+
+        for (String s1 : collection1) {
+
+            boolean isContains = calSameEleNumInc1.containsKey(s1);
+
+            if (isContains) {
+                int speEleNum = calSameEleNumInc1.get(s1) + 1;
+                calSameEleNumInc1.put(s1, speEleNum);
+            } else {
+                calSameEleNumInc1.put(s1, 1);
+            }
+        }
+
+        return calSameEleNumInc1;
     }
 }
