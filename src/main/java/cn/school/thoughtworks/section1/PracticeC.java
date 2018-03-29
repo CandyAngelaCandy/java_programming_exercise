41ed3f4080e18b639f48b918ef61eb9950ca5013
Candy <229355694@qq.com>
2018-03-29 10:55:54 +0800

    implementation class in section1 PracticeCTest

--- src/main/java/cn/school/thoughtworks/section1/PracticeC.java
+++ src/main/java/cn/school/thoughtworks/section1/PracticeC.java
@@ -7,7 +7,16 @@ import java.util.Map;
 public class PracticeC {
     List<String> collectSameElements(List<String> collection1, Map<String,List<String>> collection2) {
         //实现练习要求，并改写该行代码。
-        return null;
+
+        List<String> aList1 = new ArrayList<String>();
+        List<String> speValEleInc2 = collection2.get("value");
+
+        for (String s1 : collection1) {
+            aList1.add(s1);
+        }
+
+        aList1.retainAll(speValEleInc2);
+        return aList1;
 
     }
 }
