ea508f91d9f170c5a4f5cfceadfd8dee243dd1d2
Candy <229355694@qq.com>
2018-03-29 10:54:04 +0800

    implementation class in section1 PracticeBTest

--- src/main/java/cn/school/thoughtworks/section1/PracticeB.java
+++ src/main/java/cn/school/thoughtworks/section1/PracticeB.java
@@ -6,6 +6,14 @@ import java.util.List;
 public class PracticeB {
     List<String> collectSameElements(List<String> collection1, List<List<String>> collection2) {
         //实现练习要求，并改写该行代码。
-        return null;
+        List<String> aList1 = new ArrayList<String>();
+        List<String> subarrayEleInc2 = collection2.get(0);
+
+        for (String s1 : collection1) {
+            aList1.add(s1);
+        }
+
+        aList1.retainAll(subarrayEleInc2);
+        return aList1;
     }
 }
