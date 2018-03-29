8158abb519a016edb6481d457411ff40abc7c1a2
Candy <229355694@qq.com>
2018-03-29 14:43:48 +0800

    implementation class in section3 PracticeBTest

--- src/main/java/cn/school/thoughtworks/section3/PracticeB.java
+++ src/main/java/cn/school/thoughtworks/section3/PracticeB.java
@@ -4,9 +4,21 @@ import java.util.List;
 import java.util.Map;
 
 public class PracticeB {
-    Map<String,Integer> createUpdatedCollection(Map<String,Integer> collectionA, Map<String,List<String>> object) {
+    Map<String, Integer> createUpdatedCollection(Map<String, Integer> collectionA, Map<String, List<String>> object) {
         //实现练习要求，并改写该行代码。
+        List<String> speValEleInObj = object.get("value");
 
-        return null;
+        for (String s1 : speValEleInObj) {
+            boolean isContains = collectionA.containsKey(s1);
+
+            if (isContains) {
+                int speEleNum = collectionA.get(s1);
+                int threeNumInSpeEleNum = speEleNum / 3;
+                speEleNum -= threeNumInSpeEleNum;
+                collectionA.put(s1, speEleNum);
+            }
+        }
+
+        return collectionA;
     }
 }
